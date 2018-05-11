d6880d9cc146c73d96492390631df6fafed55717
Jonathan Bernard <jbernard@fairwaytech.com>
2018-05-11 11:33:28 -0500

    Clean slate.

--- libpit/src/main/com/jdbernard/pit/Repository.groovy
+++
@@ -1,8 +0,0 @@
-package com.jdbernard.pit
-
-public abstract class Repository {
-
-    public abstract void persist()
-    public abstract Project[] getRootProjects()
-    public abstract Project createNewProject(String name)
-}
--- libpit/src/test/com/jdbernard/pit/MockIssue.groovy
+++
@@ -1,8 +0,0 @@
-package com.jdbernard.pit
-
-public class MockIssue extends Issue {
-    public MockIssue(String id, Category c, Status s, int p) {
-        super ([id: id, category: c, status: s, priority: p])
-    }
-    public boolean delete() { return true }
-}
